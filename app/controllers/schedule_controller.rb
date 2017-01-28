aa3943859c445f617cad489d148d69ecf4a60d21
Jacky Chiu <jacky.chiu888@gmail.com>
2017-01-28 15:51:59 -0500

    Added test route to respond with one scheudle

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,2 +1,20 @@
 class ScheduleController < ApplicationController
+  
+  def index
+    test = [{
+      id: 123,
+      name: 'Breakfast',
+      time: '8:00',
+      place: 'Byte',
+      tag: [],
+      description: 'FOOD GUYSS',
+      date: Time.now
+    }]
+
+    render json: test
+  end
+
+  def create
+    @schedule = Schedule.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-root 'welcome#index'
+  root 'welcome#index'
+  get 'schedules' => 'schedule#index'
+  post 'schedules' => 'schedule#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
