6fb4f113397e94bf2d9d1cc86e4721a19523a7ba
Jacky Chiu <jacky.chiu888@gmail.com>
2017-01-28 18:55:04 -0500

    Can create schedules and add to database

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -6,7 +6,7 @@ class ScheduleController < ApplicationController
       name: 'Breakfast',
       time: '8:00',
       place: 'Byte',
-      tag: [],
+      tags: [],
       description: 'FOOD GUYSS',
       date: Time.now
     }]
@@ -15,6 +15,18 @@ class ScheduleController < ApplicationController
   end
 
   def create
-    @schedule = Schedule.new
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      render json: @schedule
+    else 
+      puts 'Not saved'
+    end
+  end
+
+  private
+  def schedule_params
+    params
+      .require(:schedule)
+      .permit(:name, :time, :place, :tags, :description, :date)
   end
 end
